b698352b464a804df6b86b9905ff5c1fb1a99cbb
Kamil Fiks <fikskamil@gmail.com>
2021-06-07 12:08:22 +0200

    angular material and cdk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BillingsComponent } from './components/billings/billings.component';
-import { HttpClientModule } from '@angular/common/http';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { BillingsFormComponent } from './components/billings-form/billings-form.component';
-import { DataTablesModule } from 'angular-datatables';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BillingsComponent} from './components/billings/billings.component';
+import {HttpClientModule} from '@angular/common/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {BillingsFormComponent} from './components/billings-form/billings-form.component';
+import {DataTablesModule} from 'angular-datatables';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { PieChartComponent } from './components/pie-chart/pie-chart.component';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import {PieChartComponent} from './components/pie-chart/pie-chart.component';
+import {MDBBootstrapModule} from 'angular-bootstrap-md';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -27,9 +28,11 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
     DataTablesModule,
     FormsModule,
     ReactiveFormsModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
